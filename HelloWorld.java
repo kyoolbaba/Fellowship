996724572792320d1cc1474e4c2b638d151c6ae1
Milan Gowda <kyoolbaba@gmail.com>
2020-04-08 16:36:56 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+class HelloWorld{
+public static void main (String []args){
+while(true){
+System.out.println("Hello World");
+try{
+Thread.sleep(60000);
+}catch(Exception e){
+System.out.print(e);
+}
+
+}
+}
+}
