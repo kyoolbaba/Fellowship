89fd2b14e88ff6c1d34161a540fbac7f410c46cf
kyoolbaba <kyoolbaba@gmail.com>
2020-04-10 20:17:39 +0530

    check 3

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld{
 public static void main (String []args){
 while(true){
-System.out.println("Hello World hey hello");
+System.out.println("Hello World");
 try{
 Thread.sleep(60000);
 }catch(Exception e){
